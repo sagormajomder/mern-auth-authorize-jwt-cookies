823ca9ab7dea24a399ef2f64476f2058ddbf12ba
Sagor Majomder <sagormajomder@gmail.com>
2026-06-25 03:09:36 +0600

    project start with typescript

---
+++ Server/src/server.ts
@@ -0,0 +1,8 @@
+import app from '@/app.js';
+import env from '@/config/env.js';
+
+const port = env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
